4266d3bab6bb5ae6cae20069504ca98763e24f36
root <anilbondala393@gmail.com>
2024-05-03 07:15:19 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const dbPath = path.join(__dirname, "posts.db");
+const app = express();
+
+app.use(express.json());
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({ filename: dbPath, driver: sqlite3.Database });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(-1);
+  }
+};
+initializeDBAndServer();
+
+//Get Posts API
+app.get("/posts/", (request, response) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid Access Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.send("Invalid Access Token");
+      } else {
+        const postsQuery = `
+            SELECT
+              *
+            FROM
+             post
+            ORDER BY
+             id;`;
+        const booksArray = await db.all(postsQuery);
+        response.send(postsArray);
+      }
+    });
+  }
+});
+
+//Get Post API
+app.get("/posts/:Id/", async (request, response) => {
+  const { id } = request.params;
+  const getPostQuery = `
+      SELECT
+       *
+      FROM
+       post 
+      WHERE
+       id = ${id};
+    `;
+  const post = await db.get(getPostQuery);
+  response.send(post);
+});
+
+//User Register API
+app.post("/users/", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+  const hashedPassword = await bcrypt.hash(request.body.password, 10);
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    const createUserQuery = `
+      INSERT INTO 
+        user (username, name, password, gender, location) 
+      VALUES 
+        (
+          '${username}', 
+          '${name}',
+          '${hashedPassword}', 
+          '${gender}',
+          '${location}'
+        )`;
+    await db.run(createUserQuery);
+    response.send(`User created successfully`);
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+
+//User Login API
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
